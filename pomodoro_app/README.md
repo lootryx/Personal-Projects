905c9cb2b97600ed521b2c7f85703222ca349c65
lootryx <lebin.nabi@gmail.com>
2020-05-07 10:52:45 -0700

    pomodoro gui app initial upload

---
+++ pomodoro_app/README.md
@@ -0,0 +1,4 @@
+This is my first attempt at a Graphic User Interface with Python and Tkinter
+
+## 1st Iteration
+Simple "Hello World" test
---
+++ pomodoro_app/pomodoro.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+
+# Creating label widget
+label = Label(root, text="Hello World")
+# Move label widget onto screen
+label.pack()
+
+
+root.mainloop()
